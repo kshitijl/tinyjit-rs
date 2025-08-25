6daebd7d5276214a87d5e627a3b6551e08dad33a
Kshitij Lauria <kslauria@gmail.com>
2025-08-25 19:01:49 -0400

    some modules inside the big file

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,9 @@
 use libc::{MAP_ANON, MAP_PRIVATE, PROT_EXEC, PROT_READ, PROT_WRITE, mmap, mprotect};
-use nom::{
-    IResult, Parser,
-    branch::alt,
-    bytes::complete::tag,
-    character::complete::{char, digit1, multispace0, multispace1},
-    combinator::{map, map_res, value},
-    multi::{many_till, separated_list1},
-    sequence::preceded,
-};
 use std::ptr;
 
 unsafe extern "C" {
     fn __clear_cache(start: *const u8, end: *const u8);
 }
-
-fn mov_immediate(register: u8, value: u16) -> u32 {
-    assert!(register < 32);
-    let base: u32 = 0xd2800000;
-    base | ((value as u32) << 5) | (register as u32)
-}
-
-fn add_reg_reg_reg(rd: u8, rn: u8, rm: u8) -> u32 {
-    let base = 0x8b000000;
-    base | ((rm as u32) << 16) | ((rn as u32) << 5) | (rd as u32)
-}
-
-fn mul_reg_reg_reg(rd: u8, rn: u8, rm: u8) -> u32 {
-    let base = 0x9b007c00;
-    base | ((rm as u32) << 16) | ((rn as u32) << 5) | (rd as u32)
-}
-
-fn sub_reg_reg_reg(rd: u8, rn: u8, rm: u8) -> u32 {
-    let base = 0xcb000000;
-    base | ((rm as u32) << 16) | ((rn as u32) << 5) | (rd as u32)
-}
-
-fn ret() -> u32 {
-    0xd65f03c0
-}
-
-fn push_literal(value: u16) -> Vec<u32> {
-    let mut instructions = Vec::new();
-
-    // Put the literal in X0: mov x0, #lit
-    instructions.push(mov_immediate(0, value));
-
-    instructions.extend(push_x0());
-
-    instructions
-}
-
-fn pop_into_reg(register: u8) -> Vec<u32> {
-    let ldr = {
-        let base: u32 = 0xf9400120;
-        base | (register as u32)
-    };
-
-    vec![
-        ldr,        // ldr x{register}, [x9]
-        0x91002129, // add x29, x29, #8
-    ]
-}
-
-fn push_reg(register: u8) -> Vec<u32> {
-    vec![
-        0xd1002129,                     // sub x9, x9, 8
-        0xf9000120 | (register as u32), // str x{register}, [x9]
-    ]
-}
-
-fn push_x0() -> Vec<u32> {
-    push_reg(0)
-}
-
-fn do_top_two_and_push(f: fn(u8, u8, u8) -> u32) -> Vec<u32> {
-    vec![
-        pop_into_reg(2),
-        pop_into_reg(1),
-        vec![f(0, 1, 2)],
-        push_x0(),
-    ]
-    .into_iter()
-    .flatten()
-    .collect()
-}
-
 #[derive(Copy, Clone, Debug)]
 enum Op {
     Add,
@@ -102,53 +21,186 @@ enum Expr {
     Over,
 }
 
-fn parse_expression(input: &str) -> IResult<&str, Vec<Expr>> {
-    separated_list1(multispace1, parse_token).parse(input)
+mod arm64 {
+    use crate::{Expr, Op};
+
+    pub fn ret() -> u32 {
+        0xd65f03c0
+    }
+
+    fn mov_immediate(register: u8, value: u16) -> u32 {
+        assert!(register < 32);
+        let base: u32 = 0xd2800000;
+        base | ((value as u32) << 5) | (register as u32)
+    }
+
+    fn add_reg_reg_reg(rd: u8, rn: u8, rm: u8) -> u32 {
+        let base = 0x8b000000;
+        base | ((rm as u32) << 16) | ((rn as u32) << 5) | (rd as u32)
+    }
+
+    fn mul_reg_reg_reg(rd: u8, rn: u8, rm: u8) -> u32 {
+        let base = 0x9b007c00;
+        base | ((rm as u32) << 16) | ((rn as u32) << 5) | (rd as u32)
+    }
+
+    fn sub_reg_reg_reg(rd: u8, rn: u8, rm: u8) -> u32 {
+        let base = 0xcb000000;
+        base | ((rm as u32) << 16) | ((rn as u32) << 5) | (rd as u32)
+    }
+
+    fn push_literal(value: u16) -> Vec<u32> {
+        let mut instructions = Vec::new();
+
+        // Put the literal in X0: mov x0, #lit
+        instructions.push(mov_immediate(0, value));
+
+        instructions.extend(push_x0());
+
+        instructions
+    }
+
+    pub fn pop_into_reg(register: u8) -> Vec<u32> {
+        let ldr = {
+            let base: u32 = 0xf9400120;
+            base | (register as u32)
+        };
+
+        vec![
+            ldr,        // ldr x{register}, [x9]
+            0x91002129, // add x29, x29, #8
+        ]
+    }
+
+    fn push_reg(register: u8) -> Vec<u32> {
+        vec![
+            0xd1002129,                     // sub x9, x9, 8
+            0xf9000120 | (register as u32), // str x{register}, [x9]
+        ]
+    }
+
+    fn cbnz(by: i32) -> u32 {
+        let base = 0xb500000a;
+        let mask = 0x00ffffff;
+
+        base | (mask & ((by as u32) << 3))
+    }
+
+    fn push_x0() -> Vec<u32> {
+        push_reg(0)
+    }
+
+    fn do_top_two_and_push(f: fn(u8, u8, u8) -> u32) -> Vec<u32> {
+        vec![
+            pop_into_reg(2),
+            pop_into_reg(1),
+            vec![f(0, 1, 2)],
+            push_x0(),
+        ]
+        .into_iter()
+        .flatten()
+        .collect()
+    }
+
+    pub fn codegen(expression: &Vec<Expr>, output: &mut Vec<u32>) {
+        for item in expression {
+            match item {
+                Expr::Number(x) => output.extend(push_literal(*x)),
+                Expr::Op(Op::Add) => output.extend(do_top_two_and_push(add_reg_reg_reg)),
+                Expr::Op(Op::Mul) => output.extend(do_top_two_and_push(mul_reg_reg_reg)),
+                Expr::Op(Op::Sub) => output.extend(do_top_two_and_push(sub_reg_reg_reg)),
+                Expr::Dup => {
+                    output.extend(pop_into_reg(0));
+                    output.extend(push_x0());
+                    output.extend(push_x0());
+                }
+
+                Expr::Swap => {
+                    output.extend(pop_into_reg(0));
+                    output.extend(pop_into_reg(1));
+                    output.extend(push_reg(0));
+                    output.extend(push_reg(1));
+                }
+
+                Expr::Over => {
+                    output.extend(pop_into_reg(0));
+                    output.extend(pop_into_reg(1));
+                    output.extend(push_reg(1));
+                    output.extend(push_reg(0));
+                    output.extend(push_reg(1));
+                }
+                Expr::Loop(body) => {
+                    output.extend(pop_into_reg(10));
+
+                    let loop_start_offset = output.len() as i32;
+                    output.push(mov_immediate(11, 1));
+                    output.push(sub_reg_reg_reg(10, 10, 11));
+
+                    codegen(&body, output);
+
+                    let loop_end_offset = output.len() as i32;
+                    let jump = cbnz(4 * (loop_start_offset - loop_end_offset));
+                    output.push(jump);
+                }
+            }
+        }
+    }
 }
 
-fn parse_token(input: &str) -> IResult<&str, Expr> {
-    alt((
-        map(parseu16, Expr::Number),
-        map(parse_op, Expr::Op),
-        parse_loop,
-        value(Expr::Dup, tag("dup")),
-        value(Expr::Swap, tag("swap")),
-        value(Expr::Over, tag("over")),
-    ))
-    .parse(input)
-}
+mod parsing {
+    use nom::{
+        IResult, Parser,
+        branch::alt,
+        bytes::complete::tag,
+        character::complete::{char, digit1, multispace0, multispace1},
+        combinator::{map, map_res, value},
+        multi::{many_till, separated_list1},
+        sequence::preceded,
+    };
 
-fn parse_loop(input: &str) -> IResult<&str, Expr> {
-    let (input, _) = tag("times")(input)?;
-    let (input, _) = multispace1(input)?;
+    use crate::{Expr, Op};
 
-    let (input, body) = many_till(
-        preceded(multispace0, parse_token),
-        preceded(multispace0, tag("end")),
-    )
-    .parse(input)?;
+    pub fn parse_expression(input: &str) -> IResult<&str, Vec<Expr>> {
+        separated_list1(multispace1, parse_token).parse(input)
+    }
 
-    Ok((input, Expr::Loop(body.0)))
-}
+    fn parse_token(input: &str) -> IResult<&str, Expr> {
+        alt((
+            map(parseu16, Expr::Number),
+            map(parse_op, Expr::Op),
+            parse_loop,
+            value(Expr::Dup, tag("dup")),
+            value(Expr::Swap, tag("swap")),
+            value(Expr::Over, tag("over")),
+        ))
+        .parse(input)
+    }
 
-fn parse_op(input: &str) -> IResult<&str, Op> {
-    alt((
-        value(Op::Add, char('+')),
-        value(Op::Sub, char('-')),
-        value(Op::Mul, char('*')),
-    ))
-    .parse(input)
-}
+    fn parse_loop(input: &str) -> IResult<&str, Expr> {
+        let (input, _) = tag("times")(input)?;
+        let (input, _) = multispace1(input)?;
 
-fn parseu16(input: &str) -> IResult<&str, u16> {
-    map_res(digit1, |s: &str| s.parse::<u16>()).parse(input)
-}
+        let (input, body) = many_till(
+            preceded(multispace0, parse_token),
+            preceded(multispace0, tag("end")),
+        )
+        .parse(input)?;
 
-fn cbnz(by: i32) -> u32 {
-    let base = 0xb500000a;
-    let mask = 0x00ffffff;
+        Ok((input, Expr::Loop(body.0)))
+    }
 
-    base | (mask & ((by as u32) << 3))
+    fn parse_op(input: &str) -> IResult<&str, Op> {
+        alt((
+            value(Op::Add, char('+')),
+            value(Op::Sub, char('-')),
+            value(Op::Mul, char('*')),
+        ))
+        .parse(input)
+    }
+
+    fn parseu16(input: &str) -> IResult<&str, u16> {
+        map_res(digit1, |s: &str| s.parse::<u16>()).parse(input)
+    }
 }
 
 fn interpret(expression: &Vec<Expr>, stack: &mut Vec<i32>) {
@@ -204,57 +256,13 @@ fn interpret(expression: &Vec<Expr>, stack: &mut Vec<i32>) {
     }
 }
 
-fn codegen(expression: &Vec<Expr>, output: &mut Vec<u32>) {
-    for item in expression {
-        match item {
-            Expr::Number(x) => output.extend(push_literal(*x)),
-            Expr::Op(Op::Add) => output.extend(do_top_two_and_push(add_reg_reg_reg)),
-            Expr::Op(Op::Mul) => output.extend(do_top_two_and_push(mul_reg_reg_reg)),
-            Expr::Op(Op::Sub) => output.extend(do_top_two_and_push(sub_reg_reg_reg)),
-            Expr::Dup => {
-                output.extend(pop_into_reg(0));
-                output.extend(push_x0());
-                output.extend(push_x0());
-            }
-
-            Expr::Swap => {
-                output.extend(pop_into_reg(0));
-                output.extend(pop_into_reg(1));
-                output.extend(push_reg(0));
-                output.extend(push_reg(1));
-            }
-
-            Expr::Over => {
-                output.extend(pop_into_reg(0));
-                output.extend(pop_into_reg(1));
-                output.extend(push_reg(1));
-                output.extend(push_reg(0));
-                output.extend(push_reg(1));
-            }
-            Expr::Loop(body) => {
-                output.extend(pop_into_reg(10));
-
-                let loop_start_offset = output.len() as i32;
-                output.push(mov_immediate(11, 1));
-                output.push(sub_reg_reg_reg(10, 10, 11));
-
-                codegen(&body, output);
-
-                let loop_end_offset = output.len() as i32;
-                let jump = cbnz(4 * (loop_start_offset - loop_end_offset));
-                output.push(jump);
-            }
-        }
-    }
-}
-
 fn main() {
     let mut input = String::new();
     std::io::stdin()
         .read_line(&mut input)
         .expect("failed to read line");
 
-    let (remainder, expression) = parse_expression(input.trim()).unwrap();
+    let (remainder, expression) = parsing::parse_expression(input.trim()).unwrap();
 
     println!(
         "Thank you for giving me a program to run. I understand it as:\n{:?}",
@@ -312,12 +320,12 @@ fn main() {
     let mov_x9_x0 = 0xaa0003e9;
     instructions.push(mov_x9_x0);
 
-    codegen(&expression, &mut instructions);
+    arm64::codegen(&expression, &mut instructions);
 
     // The final result will be at the top of the stack. Pop that result into x0
     // and return it.
-    instructions.extend(pop_into_reg(0));
-    instructions.push(ret());
+    instructions.extend(arm64::pop_into_reg(0));
+    instructions.push(arm64::ret());
 
     println!("\nHere's the raw arm64 machine code for your code:");
 
